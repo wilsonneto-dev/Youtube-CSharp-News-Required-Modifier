86787cf5aa23d3b590625597dc23161ed506e6c2
Wilson Neto B.R <contato@wilsonneto.com.br>
2023-03-18 00:54:28 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿var p = new Person { Name = "Wilson", Id = 10, Surname = "Neto" };
+
+Console.WriteLine($"{p.Id} / {p.Name}");
+
+class Person
+{   public required int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Surname { get; set; }
+}
